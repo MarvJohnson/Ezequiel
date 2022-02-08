f70dfe15f5187d2721719ba76dd5ce3e3d993254
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-08 10:25:08 -0800

    Added requestLogout to UserServices

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -7,11 +7,21 @@
 
 <script>
 import Header from '../components/Header'
+import { requestUser } from '../services/UserServices'
 
 export default {
   name: 'Home',
   components: {
     Header
+  },
+  mounted(){
+    this.requestUser()
+  },
+  methods: {
+    async requestUser(){
+      const result = await requestUser();
+      console.log(result)
+    }
   }
 }
 </script>
