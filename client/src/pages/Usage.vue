acd6f3945d9784ac7084591ddb11545f50eebdac
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-10 13:37:21 -0800

    Setup styling for usage page

--- client/src/pages/Usage.vue
+++ client/src/pages/Usage.vue
@@ -1,8 +1,47 @@
 <template>
   <div>
     <Header />
-    <h1>Usage</h1>
-    <main></main>
+    <main>
+      <section>
+        <h2>Section 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat mollitia ex eius, vero in commodi numquam maxime iure temporibus non vel pariatur, ullam, totam magnam ratione assumenda aut porro doloremque.
+        Ex sapiente sit repudiandae enim eveniet blanditiis tenetur optio veniam reiciendis corrupti quasi amet eius perspiciatis obcaecati officia, sed excepturi consectetur labore ad, cupiditate illum sint? Facilis at excepturi sequi!
+        Minima quaerat ullam nulla dicta vero voluptatum in delectus vel, beatae veritatis sunt facere at corrupti quo molestias ab recusandae aut maiores inventore corporis iste, perferendis sequi! Aperiam, voluptate libero.
+        Voluptatibus quidem animi nam ratione fuga doloribus minus enim, cum non. Ipsum, at excepturi maxime nobis accusantium nemo eaque quod, dignissimos suscipit, expedita quaerat explicabo in maiores perferendis dolores impedit!
+        Neque voluptate saepe incidunt in ducimus nam rem. Omnis recusandae odio saepe quia odit, aliquam pariatur laboriosam, doloremque minus beatae ipsa! Magnam suscipit dolore optio sed provident doloribus fugiat amet.
+        Ea nam obcaecati hic aperiam voluptate minima aspernatur molestiae dolores dolorum, odio laborum illo est eos, doloremque incidunt nemo repudiandae eligendi a? Dolorem facilis vel sint repellat aliquid? Mollitia, beatae!
+        Numquam laboriosam saepe magni suscipit sunt quidem modi ex facere dolorum nihil porro deserunt dolores fugiat ut, tempore quas hic aliquid libero, impedit non! Saepe possimus eaque vero nam quos.
+        Libero repellendus ut ea, error quos officiis, cumque fugiat voluptates dolorum doloribus ab dolore, perferendis nemo omnis eum distinctio sed similique repellat animi reiciendis odit. Delectus odit ipsa numquam dicta!
+        Minima, natus doloribus accusamus, neque incidunt vitae quibusdam ducimus unde iure iste alias voluptatibus temporibus perspiciatis dolorum! Eveniet nobis, similique officiis consequuntur quibusdam commodi accusamus architecto. Dolore minus harum nostrum!
+        Labore iusto, delectus possimus officiis rerum qui adipisci obcaecati sit sunt sint omnis debitis ea voluptas, inventore quos, culpa unde dolores veritatis excepturi commodi optio. Corrupti fugit assumenda iste corporis.</p>
+      </section>
+      <section>
+        <h2>Section 2</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo autem ratione voluptatem alias, eligendi sequi voluptatibus minima earum necessitatibus, quae corporis et optio cupiditate expedita neque nihil quos quod reprehenderit.
+        Qui, quas? Iusto vel ex recusandae porro eos rem iste suscipit totam? Quae, ex eligendi repudiandae alias molestias quo assumenda nam nisi pariatur illum atque molestiae obcaecati officia unde ullam?
+        Asperiores, fugiat repellendus vel ab tempora quia deserunt quaerat aliquid molestiae saepe nesciunt nisi iste similique, reprehenderit sunt soluta nam rem, nostrum earum eveniet non quibusdam ullam temporibus. Ullam, libero.
+        Culpa corporis, similique iure quaerat mollitia obcaecati ad explicabo totam incidunt voluptatem consequatur ratione dignissimos facilis? Magnam explicabo a quia consequuntur voluptas, enim in, beatae esse velit, deserunt culpa rerum!
+        Reiciendis odit saepe neque, expedita hic voluptatem eaque maiores alias recusandae aliquid, cumque beatae consectetur perspiciatis, cum dignissimos delectus exercitationem animi soluta ducimus eius officiis quas maxime ullam? Illo, doloremque!
+        Repudiandae iusto velit consequuntur blanditiis! Neque molestiae quaerat ratione voluptatum ducimus sunt ut illum voluptatem beatae, excepturi blanditiis repellendus unde perspiciatis consectetur, provident tempore praesentium deserunt. Nostrum itaque ducimus nulla.
+        Assumenda, qui, libero alias tempore dolore enim cumque odio hic fugiat nam magnam consectetur excepturi in, sint soluta omnis quam minus optio quisquam deleniti molestias. Rerum culpa ipsam repudiandae aperiam.
+        Illo corrupti id ex sunt ducimus expedita odit excepturi autem illum alias. Ut reprehenderit quisquam beatae repellat enim suscipit repellendus, eius, cum delectus corrupti facilis rerum perspiciatis quia autem iusto.
+        Voluptatibus, aperiam. Fugiat dignissimos ad ipsa iure ex rem fuga sint voluptatum ab! Porro obcaecati, cumque adipisci commodi consequuntur quaerat ab quibusdam distinctio reiciendis. Animi sunt aliquam pariatur blanditiis esse!
+        Dolores doloremque odit consequatur sunt ipsum modi porro vero atque. Vitae ipsam quia expedita, beatae tempora officia ex soluta libero repudiandae, quasi adipisci maiores sunt voluptatem fugiat maxime suscipit laborum!</p>
+      </section>
+      <section>
+        <h2>Section 3</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo autem ratione voluptatem alias, eligendi sequi voluptatibus minima earum necessitatibus, quae corporis et optio cupiditate expedita neque nihil quos quod reprehenderit.
+        Qui, quas? Iusto vel ex recusandae porro eos rem iste suscipit totam? Quae, ex eligendi repudiandae alias molestias quo assumenda nam nisi pariatur illum atque molestiae obcaecati officia unde ullam?
+        Asperiores, fugiat repellendus vel ab tempora quia deserunt quaerat aliquid molestiae saepe nesciunt nisi iste similique, reprehenderit sunt soluta nam rem, nostrum earum eveniet non quibusdam ullam temporibus. Ullam, libero.
+        Culpa corporis, similique iure quaerat mollitia obcaecati ad explicabo totam incidunt voluptatem consequatur ratione dignissimos facilis? Magnam explicabo a quia consequuntur voluptas, enim in, beatae esse velit, deserunt culpa rerum!
+        Reiciendis odit saepe neque, expedita hic voluptatem eaque maiores alias recusandae aliquid, cumque beatae consectetur perspiciatis, cum dignissimos delectus exercitationem animi soluta ducimus eius officiis quas maxime ullam? Illo, doloremque!
+        Repudiandae iusto velit consequuntur blanditiis! Neque molestiae quaerat ratione voluptatum ducimus sunt ut illum voluptatem beatae, excepturi blanditiis repellendus unde perspiciatis consectetur, provident tempore praesentium deserunt. Nostrum itaque ducimus nulla.
+        Assumenda, qui, libero alias tempore dolore enim cumque odio hic fugiat nam magnam consectetur excepturi in, sint soluta omnis quam minus optio quisquam deleniti molestias. Rerum culpa ipsam repudiandae aperiam.
+        Illo corrupti id ex sunt ducimus expedita odit excepturi autem illum alias. Ut reprehenderit quisquam beatae repellat enim suscipit repellendus, eius, cum delectus corrupti facilis rerum perspiciatis quia autem iusto.
+        Voluptatibus, aperiam. Fugiat dignissimos ad ipsa iure ex rem fuga sint voluptatum ab! Porro obcaecati, cumque adipisci commodi consequuntur quaerat ab quibusdam distinctio reiciendis. Animi sunt aliquam pariatur blanditiis esse!
+        Dolores doloremque odit consequatur sunt ipsum modi porro vero atque. Vitae ipsam quia expedita, beatae tempora officia ex soluta libero repudiandae, quasi adipisci maiores sunt voluptatem fugiat maxime suscipit laborum!</p>
+      </section>
+    </main>
     <Footer />
   </div>
 </template>
@@ -22,6 +61,19 @@ export default {
 
 <style scoped>
   main {
-    height: 100vh;
+    padding: 10rem 0;
+  }
+
+  section {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+
+  section + section {
+    margin-top: 5rem;
+  }
+
+  section p {
+    text-align: justify;
   }
 </style>
