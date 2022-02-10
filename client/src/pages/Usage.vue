b123a6c8c2c0c395b02f1fdb20d5aecfd243eb3d
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-09 17:31:06 -0800

    Added header and footer to all pages that need them

--- client/src/pages/Usage.vue
+++ client/src/pages/Usage.vue
@@ -2,20 +2,26 @@
   <div>
     <Header />
     <h1>Usage</h1>
+    <main></main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default {
   name: 'Usage',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  main {
+    height: 100vh;
+  }
 </style>
