01c0e4acb52e68e72feb5ad2887c4016b43a29ce
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-10 11:11:04 -0800

    Wired up endpoint for leaving dms

--- ezequiel/urls.py
+++ ezequiel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
-  re_path(r'^.*\/?$', views.vue_render, name='vue_rener')
+  path('remove_direct_message/<int:pk>', views.DirectMessageDetail.as_view(), name='remove_direct_message'),
+  re_path(r'^.*\/?$', views.vue_render, name='vue_rener'),
 ]
--- ezequiel/views.py
+++ ezequiel/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User, DirectMessage
+from .serializers import UserSerializer, DirectMessageSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 def vue_render(request):
   return render(request, 'ezequiel/base.html')
+
+class DirectMessageDetail(APIView):
+  authentication_classes = [TokenAuthentication]
+  permission_classes = [IsAuthenticated]
+  
+  def delete(self, request, pk, format=None):
+    user = request.user
+
+    if user != None:
+      dm = DirectMessage.objects.get(id=pk)
+      user.direct_messages.remove(dm)
+      return Response(None, status=200)
+    
+    return Response(None, status=401)
+
+
