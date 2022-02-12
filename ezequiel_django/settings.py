684255b831cb55fcdb848a8d884ecd2d464536b8
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-12 03:21:00 -0800

    Updated config for deployment

--- ezequiel_django/settings.py
+++ ezequiel_django/settings.py
@@ -173,7 +173,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.getenv('REDIS_HOST'), os.getenv('REDIS_PORT'))]
+            'hosts': [(os.getenv('REDIS_URL'))],
         }
     }
 }
