bae4e977951dd110907bf44e3b553f2e66f887ee
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-12 01:47:10 -0800

    Updated vue globals

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.VUE_APP_MODE === 'production'
-    ? `${window.location.origin}/api/v1/`
-    : 'http://localhost:8000/api/v1';
+export const BASE_URL = `${window.location.origin}/api/v1/`;
