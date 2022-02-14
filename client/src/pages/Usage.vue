1338aceaaef0a1317fb5dca9593ea6b227e15c1d
menthus123 <MarvelJohnson@Hotmail.com>
2022-02-14 05:20:31 -0800

    Added content to usage page

--- client/src/pages/Usage.vue
+++ client/src/pages/Usage.vue
@@ -3,43 +3,30 @@
     <Header />
     <main>
       <section>
-        <h2>Section 1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat mollitia ex eius, vero in commodi numquam maxime iure temporibus non vel pariatur, ullam, totam magnam ratione assumenda aut porro doloremque.
-        Ex sapiente sit repudiandae enim eveniet blanditiis tenetur optio veniam reiciendis corrupti quasi amet eius perspiciatis obcaecati officia, sed excepturi consectetur labore ad, cupiditate illum sint? Facilis at excepturi sequi!
-        Minima quaerat ullam nulla dicta vero voluptatum in delectus vel, beatae veritatis sunt facere at corrupti quo molestias ab recusandae aut maiores inventore corporis iste, perferendis sequi! Aperiam, voluptate libero.
-        Voluptatibus quidem animi nam ratione fuga doloribus minus enim, cum non. Ipsum, at excepturi maxime nobis accusantium nemo eaque quod, dignissimos suscipit, expedita quaerat explicabo in maiores perferendis dolores impedit!
-        Neque voluptate saepe incidunt in ducimus nam rem. Omnis recusandae odio saepe quia odit, aliquam pariatur laboriosam, doloremque minus beatae ipsa! Magnam suscipit dolore optio sed provident doloribus fugiat amet.
-        Ea nam obcaecati hic aperiam voluptate minima aspernatur molestiae dolores dolorum, odio laborum illo est eos, doloremque incidunt nemo repudiandae eligendi a? Dolorem facilis vel sint repellat aliquid? Mollitia, beatae!
-        Numquam laboriosam saepe magni suscipit sunt quidem modi ex facere dolorum nihil porro deserunt dolores fugiat ut, tempore quas hic aliquid libero, impedit non! Saepe possimus eaque vero nam quos.
-        Libero repellendus ut ea, error quos officiis, cumque fugiat voluptates dolorum doloribus ab dolore, perferendis nemo omnis eum distinctio sed similique repellat animi reiciendis odit. Delectus odit ipsa numquam dicta!
-        Minima, natus doloribus accusamus, neque incidunt vitae quibusdam ducimus unde iure iste alias voluptatibus temporibus perspiciatis dolorum! Eveniet nobis, similique officiis consequuntur quibusdam commodi accusamus architecto. Dolore minus harum nostrum!
-        Labore iusto, delectus possimus officiis rerum qui adipisci obcaecati sit sunt sint omnis debitis ea voluptas, inventore quos, culpa unde dolores veritatis excepturi commodi optio. Corrupti fugit assumenda iste corporis.</p>
+        <h2>Why build this?</h2>
+        <p>The 3 months I recently spent using [REDACTED] made me realise just how much I'd enjoy trying to make something better. Plus, it feels good using tools that you built yourself!</p>
       </section>
       <section>
-        <h2>Section 2</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo autem ratione voluptatem alias, eligendi sequi voluptatibus minima earum necessitatibus, quae corporis et optio cupiditate expedita neque nihil quos quod reprehenderit.
-        Qui, quas? Iusto vel ex recusandae porro eos rem iste suscipit totam? Quae, ex eligendi repudiandae alias molestias quo assumenda nam nisi pariatur illum atque molestiae obcaecati officia unde ullam?
-        Asperiores, fugiat repellendus vel ab tempora quia deserunt quaerat aliquid molestiae saepe nesciunt nisi iste similique, reprehenderit sunt soluta nam rem, nostrum earum eveniet non quibusdam ullam temporibus. Ullam, libero.
-        Culpa corporis, similique iure quaerat mollitia obcaecati ad explicabo totam incidunt voluptatem consequatur ratione dignissimos facilis? Magnam explicabo a quia consequuntur voluptas, enim in, beatae esse velit, deserunt culpa rerum!
-        Reiciendis odit saepe neque, expedita hic voluptatem eaque maiores alias recusandae aliquid, cumque beatae consectetur perspiciatis, cum dignissimos delectus exercitationem animi soluta ducimus eius officiis quas maxime ullam? Illo, doloremque!
-        Repudiandae iusto velit consequuntur blanditiis! Neque molestiae quaerat ratione voluptatum ducimus sunt ut illum voluptatem beatae, excepturi blanditiis repellendus unde perspiciatis consectetur, provident tempore praesentium deserunt. Nostrum itaque ducimus nulla.
-        Assumenda, qui, libero alias tempore dolore enim cumque odio hic fugiat nam magnam consectetur excepturi in, sint soluta omnis quam minus optio quisquam deleniti molestias. Rerum culpa ipsam repudiandae aperiam.
-        Illo corrupti id ex sunt ducimus expedita odit excepturi autem illum alias. Ut reprehenderit quisquam beatae repellat enim suscipit repellendus, eius, cum delectus corrupti facilis rerum perspiciatis quia autem iusto.
-        Voluptatibus, aperiam. Fugiat dignissimos ad ipsa iure ex rem fuga sint voluptatum ab! Porro obcaecati, cumque adipisci commodi consequuntur quaerat ab quibusdam distinctio reiciendis. Animi sunt aliquam pariatur blanditiis esse!
-        Dolores doloremque odit consequatur sunt ipsum modi porro vero atque. Vitae ipsam quia expedita, beatae tempora officia ex soluta libero repudiandae, quasi adipisci maiores sunt voluptatem fugiat maxime suscipit laborum!</p>
+        <h2>How will you do it?</h2>
+        <p>One of the beautiful things about the age we live in, is just how many resources are available online.</p>
+        <p>In combination with the WebRTC API, I'm using WebSockets to establish connections between clients all around the world. These tools make streaming media (e.g. video & audio)
+          significantly easier.
+          Django, the web framework I'm using for my back-end, is incredibly robust and empowers developers like myself to create apps such as this.
+        </p>
       </section>
       <section>
-        <h2>Section 3</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo autem ratione voluptatem alias, eligendi sequi voluptatibus minima earum necessitatibus, quae corporis et optio cupiditate expedita neque nihil quos quod reprehenderit.
-        Qui, quas? Iusto vel ex recusandae porro eos rem iste suscipit totam? Quae, ex eligendi repudiandae alias molestias quo assumenda nam nisi pariatur illum atque molestiae obcaecati officia unde ullam?
-        Asperiores, fugiat repellendus vel ab tempora quia deserunt quaerat aliquid molestiae saepe nesciunt nisi iste similique, reprehenderit sunt soluta nam rem, nostrum earum eveniet non quibusdam ullam temporibus. Ullam, libero.
-        Culpa corporis, similique iure quaerat mollitia obcaecati ad explicabo totam incidunt voluptatem consequatur ratione dignissimos facilis? Magnam explicabo a quia consequuntur voluptas, enim in, beatae esse velit, deserunt culpa rerum!
-        Reiciendis odit saepe neque, expedita hic voluptatem eaque maiores alias recusandae aliquid, cumque beatae consectetur perspiciatis, cum dignissimos delectus exercitationem animi soluta ducimus eius officiis quas maxime ullam? Illo, doloremque!
-        Repudiandae iusto velit consequuntur blanditiis! Neque molestiae quaerat ratione voluptatum ducimus sunt ut illum voluptatem beatae, excepturi blanditiis repellendus unde perspiciatis consectetur, provident tempore praesentium deserunt. Nostrum itaque ducimus nulla.
-        Assumenda, qui, libero alias tempore dolore enim cumque odio hic fugiat nam magnam consectetur excepturi in, sint soluta omnis quam minus optio quisquam deleniti molestias. Rerum culpa ipsam repudiandae aperiam.
-        Illo corrupti id ex sunt ducimus expedita odit excepturi autem illum alias. Ut reprehenderit quisquam beatae repellat enim suscipit repellendus, eius, cum delectus corrupti facilis rerum perspiciatis quia autem iusto.
-        Voluptatibus, aperiam. Fugiat dignissimos ad ipsa iure ex rem fuga sint voluptatum ab! Porro obcaecati, cumque adipisci commodi consequuntur quaerat ab quibusdam distinctio reiciendis. Animi sunt aliquam pariatur blanditiis esse!
-        Dolores doloremque odit consequatur sunt ipsum modi porro vero atque. Vitae ipsam quia expedita, beatae tempora officia ex soluta libero repudiandae, quasi adipisci maiores sunt voluptatem fugiat maxime suscipit laborum!</p>
+        <h2>What do you have planned?</h2>
+        <p>I encourage any of the curious minds reading this to visit the GitHub repo for this project, as that is where the most up-to-date information
+          will be found. That being said, there are a few items being worked on at the moment:
+        </p>
+        <ul>
+            <li>direct messaging,</li>
+            <li>screen sharing,</li>
+            <li>media controls (e.g. volume sliders, background filters, etc),</li>
+            <li>admin controls and private rooms,</li>
+            <li>and adding friends</li>
+        </ul>
+        <p>in no particular order.</p>
       </section>
     </main>
     <Footer />
@@ -64,9 +51,37 @@ export default {
     padding: 10rem 0;
   }
 
+  @keyframes slide-fade-in {
+    from {
+      transform: translateX(-10vw);
+      /* left: -20vw; */
+      opacity: 0;
+    }
+
+    to {
+      transform: translateX(0);
+      /* left: 0; */
+      opacity: 1;
+    }
+  }
+
   section {
+    opacity: 0;
+    position: relative;
     max-width: 700px;
     margin: 0 auto;
+  }
+
+  section:nth-child(1) {
+    animation: slide-fade-in 2s ease-in-out forwards;
+  }
+
+  section:nth-child(2) {
+    animation: slide-fade-in 2s 0.5s ease-in-out forwards;
+  }
+
+  section:nth-child(3) {
+    animation: slide-fade-in 2s 1s ease-in-out forwards;
   }
 
   section + section {
@@ -83,5 +98,9 @@ export default {
     text-align: justify;
     font-size: 1.5rem;
     line-height: 2rem;
+  }
+
+  section ul {
+    font-size: 1.5rem;
   }
 </style>
